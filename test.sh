6cd5ebb077c22b4cf9620bec5c5f1a29f1768f71
alcover <13189858+alcover@users.noreply.github.com>
2020-06-20 19:05:07 +0200

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+echo "runc hello.c Bob"
 ./runc hello.c Bob
-./runc 'printf("%s\n", "Hello Bob!");'
+
+# code='printf("%s\n", "Hello Bob!");'
+echo
+echo "runc 'printf(\"%s\\\n\", \"Hello Bob\");'"
+./runc 'printf("%s\n", "Hello Bob");'
